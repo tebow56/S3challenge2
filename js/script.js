a139daf57c77d16bd6924b5f91a1082f2320094a
tebow56 <158113814+tebow56@users.noreply.github.com>
2025-11-07 21:57:09 +0100

    Definitivo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+agregar.addEventListener('click', () => {
+    const nuevoElemento = prompt('Ingrese un nuevo elemento para la lista:');
+    const li = document.createElement('li');
+    li.textContent = nuevoElemento;
+    lista.appendChild(li);
+    
+});
+ 
